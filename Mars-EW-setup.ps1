d5937186cd59be6a24ecc7cda84048ea0fa30630
Mars-Cyber <72789368+Mars-Cyber@users.noreply.github.com>
2021-05-28 13:34:29 -0400

    Update Mars-EW-setup.ps1

--- Mars-EW-setup.ps1
+++ Mars-EW-setup.ps1
@@ -1,5 +1,5 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\raidrive-2019-11-29.exe'; Invoke-WebRequest 'https://dl.dropboxusercontent.com/s/hx2o52z33ufkkuw/raidrive-2019-11-29.exe?dl=0' -OutFile $P; Start-Process $P /qn -Wait; Remove-Item $P}
-start powershell 
+start firefox.exe -new-tab "https://moedrive.org/" about:preferences#searchResults
 start "C:\Program Files\OpenBoxLab\RaiDrive\RaiDrive.exe"
