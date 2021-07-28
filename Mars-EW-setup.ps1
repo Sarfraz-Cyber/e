5a6c5bde0c49e573c8467c2ec7257496991a741b
Mars-Cyber <72789368+Mars-Cyber@users.noreply.github.com>
2021-07-28 09:21:45 +0530

    Update Mars-EW-setup.ps1

--- Mars-EW-setup.ps1
+++ Mars-EW-setup.ps1
@@ -3,5 +3,6 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\raidrive-2019-11-29.exe'; Invoke-WebRequest 'https://dl.dropboxusercontent.com/s/hx2o52z33ufkkuw/raidrive-2019-11-29.exe?dl=0' -OutFile $P; Start-Process $P /qn -Wait; Remove-Item $P}
 choco install wget
 choco install unrar
+choco install qbittorrent
 start powershell
 start "C:\Program Files\OpenBoxLab\RaiDrive\RaiDrive.exe"
